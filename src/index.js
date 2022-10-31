77c71de3bb80541723b0a5a979880e409e50af10
ClaudiaD86 <111800917+ClaudiaD86@users.noreply.github.com>
2022-10-31 14:09:11 +0100

    reorganizing and renaming images

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.time * 1000);
   iconElement.setAttribute(
     "src",
-    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+    `images/${response.data.condition.icon}.png`
   );
   iconElement.setAttribute("alt", response.data.condition.icon);
 }
